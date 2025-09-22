248387be1708753dea98da089d6c6a0cbb41d697
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-09-21 21:02:59 -0600

    Correccion de js

--- CalculoDNI.js
+++ CalculoDNI.js
@@ -1,18 +1,25 @@
-const btnVerificar=document.getElementById("button-verificacion");
-  var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-  btnVerificar.addEventListener("click",()=>{alert(validacionDNI)})
-  function validacionDNI (){
-    const numDNI= document.getElementById("Text-numeroDNI").value;
-    const letraDNI= document.getElementById("text-letraDNI").value;
-    if(numDNI.trim()=== "" || letraDNI.trim()=== "" || !Number.isInteger(Number(numDNI))){
-        return "Verifique la entrada"
-    }
-    if (Number(numDNI)<0 || Number(numDNI)>99999999) {
-        return "Formato de numero fuera de rango"
-    }
-    if (letraDNI.trim.toUpperCase()=== letraDNI[Number(numDNI)%23]) {
-    return "El DNI y la letra es correcta"        
-    } else {
-        return "El DNI y la letra no es correcta"
-    }   
+const btnVerificar = document.getElementById("button-verificacion");
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+
+btnVerificar.addEventListener("click", () => {
+  alert(validacionDNI());
+});
+
+function validacionDNI() {
+  const numDNI = document.getElementById("text-numeroDNI").value;
+  const letraDNI = document.getElementById("text-letraDNI").value;
+
+  if (numDNI.trim() === "" || letraDNI.trim() === "" || !Number.isInteger(Number(numDNI))) {
+    return " Verifique la entrada";
   }
+
+  if (Number(numDNI) < 0 || Number(numDNI) > 99999999) {
+    return " Formato de número fuera de rango";
+  }
+
+  if (letraDNI.trim().toUpperCase() === letras[Number(numDNI) % 23]) {
+    return " El DNI y la letra son correctos";
+  } else {
+    return " El DNI y la letra no son correctos";
+  }
+}
